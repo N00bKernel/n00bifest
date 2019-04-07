b10a685ca55370feb72b60656e338b990a79075d
Chandan Kumar Mandal <chankruze@gmail.com>
2019-04-07 11:33:12 +0530

    Update Build Script
    Anykernel2 was unable to split boot image so i moved from anykernel2 to lazyflasher.
    These are the few changes due to changing flashing template.

--- BuildN00bKernel.sh
+++ BuildN00bKernel.sh
@@ -35,9 +35,9 @@ RELEASE_DIR=~/AndroidSystemDev/potter/N00bReleases
 # OTA server dir
 OTA_DIR=/var/www/html/N00bKernelDownloads
 # N00bKernel update dir (on device)
-DEVICE_UPDATE_DIR=/sdcard/N00bKernelUpdate/
+DEVICE_UPDATE_DIR=/sdcard/N00bKernelUpdate
 # Toolchain dir
-TOOLCHAIN_DIR=~/AndroidSystemDev/aarch64-linux-android-4.9/bin/
+TOOLCHAIN_DIR=~/AndroidSystemDev/aarch64-linux-android-4.9/bin
 # ARCH & SUBARCH
 ARCH=arm64
 SUBARCH=arm64
@@ -62,24 +62,20 @@ make O=$WORK_DIR potter_defconfig
 # start build
 make O=$WORK_DIR -j$(nproc --ignore 4)
 sleep 2
-# copy compiled kernel to shipping directory
-cp $KERNEL_DIR/Image.gz $SHIPPING_DIR/
-sleep 2
 # change directory to shipping directory
 cd $SHIPPING_DIR/
-# remove old zip (here kept as backup)
+# remove old kernel & zip (here kept as backup)
+rm Image.gz
 rm N00bKernel-*.zip
+# copy compiled kernel to shipping directory
+cp $KERNEL_DIR/Image.gz $SHIPPING_DIR/
 # archive and make flashable zip
-zip -r9 N00bKernel-$(date +"%Y-%m-%d").zip * -x .* .git .git README.md *placeholder N00bKernel-*.zip
+make
 sleep 5
-if [ -f $SHIPPING_DIR/N00bKernel-$(date +"%Y-%m-%d").zip ]; then
-    echo "[W] Deleting Today's Previous Build !"
-    rm $SHIPPING_DIR/N00bKernel-$(date +"%Y-%m-%d").zip
-fi
 # copy to release dir
-cp $SHIPPING_DIR/N00bKernel-$(date +"%Y-%m-%d").zip $RELEASE_DIR/
+cp $SHIPPING_DIR/N00bKernel-*.zip $RELEASE_DIR/
 # copy to server
-sudo cp $SHIPPING_DIR/N00bKernel-$(date +"%Y-%m-%d").zip $OTA_DIR/
+sudo cp $SHIPPING_DIR/N00bKernel-*.zip $OTA_DIR/
 #
 # Pushing Update To Device Via ADB
 #
@@ -90,7 +86,8 @@ sleep 5
 adb connect $(ip route show | grep "default via" | cut -d" " -f3)
 sleep 5
 adb devices
-adb push N00bKernel-$(date +"%Y-%m-%d").zip $DEVICE_UPDATE_DIR
+sleep 2
+adb push N00bKernel-*.zip $DEVICE_UPDATE_DIR/
 sleep 5
 adb reboot recovery
 # change directory back to kernel source (displacement = 0)
